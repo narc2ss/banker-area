4cb14c536f117bd439826232e65e05aa39e26d1e
narc2ss <aksrb5149@gmail.com>
2020-07-15 17:08:06 +0900

    for attach redux thunk

---
+++ src/containers/CounterContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { useSelector, useDispatch } from "react-redux";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => dispatch(increaseAsync());
+  const onDecrease = () => dispatch(decreaseAsync());
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
 
 import rootReducer from "./modules";
 import Root from "./client/Root";
